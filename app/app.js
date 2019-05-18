902ada947c88de5d4c5de8d23f699729328620af
xiaoguoping <gpxiao118@163.com>
2019-05-18 22:13:53 +0800

    update

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-// app.js
 const Koa = require('koa')
 const Router = require('koa-router')
 
@@ -6,18 +5,16 @@ const app = new Koa()
 const router = new Router()
 
 app.use(async (ctx, next) => {
-  console.log("Url: ", ctx.url)
+  console.log('Url: ', ctx.url)
 
   await next()
 })
 
 router.get('*', async ctx => {
-  ctx.body = 'Hello World'
+  ctx.body = 'Hello Nodejs Debug'
 })
-
 
 app.use(router.routes())
 
-
-app.listen(3000);
+app.listen(3000)
 console.log('listening on port 3000')
---
+++ app/debug.js
@@ -0,0 +1,6 @@
+function compute (a, b) {
+  return a + b
+}
+
+let result = compute(2, 3)
+console.log(result)
